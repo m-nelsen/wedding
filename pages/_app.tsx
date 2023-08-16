01d7d1829e23f947c72364a82811d0fd0ec511fa
m-nelsen <mattnelsen95@gmail.com>
2023-08-15 23:25:00 -0700

    Adding bootstrap to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/main.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }
