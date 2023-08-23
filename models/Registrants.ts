97625125cb7b3ba1b505f985b5679aadfca12c52
m-nelsen <mattnelsen95@gmail.com>
2023-08-23 09:24:19 -0700

    Adding more fields to registrants schema

--- models/Registrants.ts
+++ models/Registrants.ts
@@ -1,15 +1,44 @@
 import mongoose from "mongoose";
 
+const shortTextInputLength = 80;
+const longTextInputLength = 500;
+
 const RegistrantSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: [true, "Please provide your first name."],
-    maxlength: [80, "Name cannot be more than 80 characters."],
+    maxlength: [
+      shortTextInputLength,
+      `Name cannot be more than ${shortTextInputLength} characters.`,
+    ],
   },
   lastName: {
     type: String,
     required: [true, "Please provide your last name."],
-    maxlength: [80, "Name cannot be more than 80 characters."],
+    maxlength: [
+      shortTextInputLength,
+      `Name cannot be more than ${shortTextInputLength} characters.`,
+    ],
+  },
+  attendanceStatus: {
+    type: Boolean,
+    required: [true, "Will you be attending?"],
+  },
+  preferredEntree: {
+    type: String,
+    required: [true, "Please provide your preferred entree."],
+    maxlength: [
+      shortTextInputLength,
+      `Entree cannot be more than ${shortTextInputLength} characters.`,
+    ],
+  },
+  dietaryRestrictions: {
+    type: String,
+    required: [false],
+    maxlength: [
+      longTextInputLength,
+      `Entree cannot be more than ${longTextInputLength} characters.`,
+    ],
   },
 });
 
