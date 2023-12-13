f2b2239c559f962bb704ac528294ddf88dc54f38
m-nelsen <mattnelsen95@gmail.com>
2023-12-13 10:23:18 -0700

    Adding back pre-migration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,199 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Countdown from "./components/Countdown";
+import RsvpForm from "./components/RsvpForm";
+import "./scss/main.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section
+        className="container-md position-relative g-0"
+        style={{
+          minHeight: "600px",
+          marginTop: "2rem",
+          marginBottom: "18rem",
+        }}
+      >
+        <div className="position-absolute h-100 w-100" style={{ zIndex: "-1" }}>
+          <img
+            src="/holding-hands.webp"
+            style={{
+              objectFit: "cover",
+              objectPosition: "60% 30%",
+            }}
+            className="w-100 h-100 rounded"
+            alt="Matt and Madelyne holding hands with engagement ring in view"
+          />
+        </div>
+        <div
+          className="text-center d-flex flex-column justify-content-center align-items-center"
+          style={{ height: "600px" }}
+        >
+          <h1 className="display-3">Matthew & Madelyne</h1>
+          <p className="text-uppercase middle-line w-50">Save the date</p>
+          <p className="display-6 mb-4">May 11, 2024</p>
+          <a
+            className="btn btn-secondary px-5 py-2 rounded-1 bg-light fs-4"
+            href="#rsvp"
+          >
+            RSVP
+          </a>
+          <div
+            className="position-absolute h-100 sm-width-100"
+            style={{
+              top: "91%",
+              margin: "auto",
+              zIndex: "10",
+            }}
+          >
+            <Countdown />
+
+            <img
+              src="/hanging-leaves.webp"
+              style={{
+                objectFit: "contain",
+                maxWidth: "800px",
+                maxHeight: "200px",
+                transform: "translatey(-60px)",
+                zIndex: "-10",
+                position: "relative",
+              }}
+              className="px-2 w-100"
+              alt="Leaves hanging beneath the wedding countdown clock"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="container-md position-relative join-us">
+        <div className="overlay rounded"></div>
+        <div className="row m-0 w-100">
+          <div className="col-12 col-md-4 p-0 m-0 mr-md-2">
+            <img
+              src="/windmill.webp"
+              alt="Matt and Madelyne holding hands with engagement ring in view"
+              className="w-100 h-100 rounded"
+              style={{
+                objectFit: "cover",
+                objectPosition: "18% 0",
+              }}
+            />
+          </div>
+          <div className="col-12 col-md-8 p-0 m-0 p-md-1 position-relative">
+            <div className="location-info g-0">
+              <div
+                className="position-absolute w-100 d-flex"
+                style={{ top: "-90px" }}
+              >
+                <img
+                  src="/dual-rings.webp"
+                  className="mx-auto"
+                  style={{ height: "auto", width: "250px" }}
+                  alt="Two intertwined wedding rings"
+                />
+              </div>
+              <div className="text-center bg-light border border-secondary border-1 rounded py-5">
+                <h2 className="mt-3">Join us at Windmill House</h2>
+                <p className="fs-5 mb-2">Sunday May 11th, 2024 at 3:00pm</p>
+                <p className="fs-5">1460 W Rd 4 N, Chino Valley, AZ 86323</p>
+              </div>
+            </div>
+            <div className="row g-0 pt-1">
+              <img
+                src="/ceremony.webp"
+                alt="Arch and chairs decorated for wedding ceremony"
+                className="h-100 rounded"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section
+        className="container position-relative"
+        style={{ marginBottom: "12rem" }}
+      >
+        <div className="position-absolute h-100 w-100" style={{ zIndex: "-1" }}>
+          <img
+            src="/heart-and-leaves.webp"
+            style={{
+              objectFit: "cover",
+              objectPosition: "60% 50%",
+              opacity: "6%",
+            }}
+            className="w-100 h-100 rounded"
+            alt="Hearts and leaves"
+          />
+        </div>
+        <div className="text-center">
+          <h2>Registry</h2>
+          <p className="m-0">
+            If you want to donate to our honeymoon fund, please visit our
+            registry.
+          </p>
+          <a
+            href="https://withjoy.com/madelyne-and-matthew/registry"
+            className="text-info"
+          >
+            https://withjoy.com/madelyne-and-matthew/registry
+          </a>
+          <p className="mt-4">
+            There will be a donation box at our wedding too.
+          </p>
+        </div>
+      </section>
+
+      <section
+        id="rsvp"
+        className="container bg-light shadow rounded mt-5 p-2"
+        style={{ marginBottom: "10rem" }}
+      >
+        <div
+          className="position-relative w-75 h-75"
+          style={{ left: "-9rem", top: "-8rem", zIndex: "-10" }}
+        >
+          <img
+            src="/curved-branch.webp"
+            style={{
+              objectFit: "cover",
+              transform: "scale(-1, 1)",
+              position: "absolute",
+              height: "300px",
+              width: "300px",
+            }}
+            alt="Branch of leaves curving around the RSVP form"
+          />
+        </div>
+        <div className="border border-secondary border-1 rounded p-3">
+          <h2 className="text-center">RSVP</h2>
+          <RsvpForm />
+        </div>
+        <div
+          className="position-relative w-50 h-50"
+          style={{
+            right: "-92%",
+            top: "-8rem",
+            zIndex: "10",
+          }}
+        >
+          <img
+            src="/flower.webp"
+            style={{
+              objectFit: "cover",
+              position: "absolute",
+              height: "200px",
+              width: "200px",
+            }}
+            alt="Flower in the bottom-right corner of the RSVP form"
+          />
+        </div>
+      </section>
+
+      <section className="container d-flex justify-content-center">
+        <p>2024 M&amp;M Wedding</p>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/RsvpForm/index.tsx
@@ -0,0 +1,321 @@
+import { FC, Fragment, useEffect } from "react";
+import {
+  useForm,
+  SubmitHandler,
+  FieldValues,
+  useFieldArray,
+} from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  attendanceStatus: boolean;
+  preferredEntree: string;
+  dietaryRestrictions: string;
+};
+
+const trashIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    className="bi bi-trash"
+    viewBox="0 0 16 16"
+  >
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+  </svg>
+);
+
+const RsvpForm: FC = () => {
+  const formValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    attendanceStatus: true,
+    preferredEntree: "",
+    dietaryRestrictions: "",
+  };
+
+  const { register, handleSubmit, formState, control, watch } = useForm<any>({
+    defaultValues: formValues,
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  });
+
+  const { errors, isSubmitting } = formState;
+
+  const { fields, append, remove } = useFieldArray<any>({
+    name: "guests",
+    control,
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data: FieldValues) => {
+    console.log("submit | data: ", data);
+
+    fetch("/api/registrants", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            {...register(`firstName`, {
+              required: "First name is required",
+            })}
+            className="form-control d-inline w-50 rounded-1"
+            placeholder="First name"
+          />
+          <input
+            {...register(`lastName`, {
+              required: "Last name is required",
+            })}
+            className="form-control d-inline w-50 rounded-1"
+            placeholder="Last name"
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name={`firstName`}
+            message="First name is required"
+            as={<div className="invalid-feedback d-inline-block w-50"></div>}
+          />
+          <ErrorMessage
+            errors={errors}
+            name={`lastName`}
+            message="Last name is required"
+            as={<div className="invalid-feedback d-inline-block w-50"></div>}
+          />
+
+          <input
+            {...register(`email`, {
+              required: "Email is required",
+            })}
+            className="form-control mt-4 rounded-1"
+            type="email"
+            placeholder="Email"
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name={`email`}
+            message="Email is required"
+            as={<div className="invalid-feedback d-inline-block"></div>}
+          />
+
+          <label className="mt-4 d-block">
+            Will you be attending the wedding?
+          </label>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              id="acceptRadio"
+              {...register(`attendanceStatus`, {
+                required: true,
+              })}
+              value="true"
+            />
+            <label className="form-check-label" htmlFor="acceptRadio">
+              Yes
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              id="declineRadio"
+              {...register(`attendanceStatus`, {
+                required: "Response is required",
+              })}
+              value="false"
+            />
+            <label className="form-check-label" htmlFor="declineRadio">
+              No
+            </label>
+          </div>
+
+          <ErrorMessage
+            errors={errors}
+            name={`attendanceStatus`}
+            message="Attendence response is required"
+            as={<div className="invalid-feedback d-inline-block"></div>}
+          />
+
+          <label className="mt-4 d-block">What is your preferred entree?</label>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              id="primeRibRadio"
+              {...register(`preferredEntree`, {
+                required: false,
+              })}
+              value="Prime Rib"
+            />
+            <label className="form-check-label" htmlFor="primeRibRadio">
+              Prime Rib
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              id="bbqChickenRadio"
+              {...register(`preferredEntree`, {
+                required: false,
+              })}
+              value="BBQ Chicken"
+            />
+            <label className="form-check-label" htmlFor="bbqChickenRadio">
+              BBQ Chicken
+            </label>
+          </div>
+
+          <label className="mt-4">Please list any dietary restrictions:</label>
+          <textarea
+            className="form-control rounded-1"
+            id="dietaryRestrictionsTextArea"
+            rows={3}
+            {...register(`dietaryRestrictions`, {
+              required: false,
+            })}
+          ></textarea>
+
+          {fields.map((field, index) => {
+            return (
+              <Fragment key={field.id}>
+                <hr className="my-4" />
+                <div className="d-flex align-items-center justify-content-between my-2">
+                  <h3 className="fs-5 m-0">{`Guest ${index + 1}`}</h3>
+                  <div
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      remove(index);
+                    }}
+                  >
+                    {trashIcon}
+                  </div>
+                </div>
+                <input
+                  {...register(`guests.${index}.firstName`, {
+                    required: "First name is required",
+                  })}
+                  className="form-control d-inline w-50 rounded-1"
+                  placeholder="First name"
+                />
+                <input
+                  {...register(`guests.${index}.lastName`, {
+                    required: "Last name is required",
+                  })}
+                  className="form-control d-inline w-50 rounded-1"
+                  placeholder="Last name"
+                />
+
+                <ErrorMessage
+                  errors={errors}
+                  name={`guests.${index}.firstName`}
+                  message="First name is required"
+                  as={
+                    <div className="invalid-feedback d-inline-block w-50"></div>
+                  }
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name={`guests.${index}.lastName`}
+                  message="Last name is required"
+                  as={
+                    <div className="invalid-feedback d-inline-block w-50"></div>
+                  }
+                />
+                <label className="mt-4 d-block">
+                  What is their preferred entree?
+                </label>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    id="primeRibRadio"
+                    {...register(`guests.${index}.preferredEntree`, {
+                      required: false,
+                    })}
+                    value="Prime Rib"
+                  />
+                  <label className="form-check-label" htmlFor="primeRibRadio">
+                    Prime Rib
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    id="bbqChickenRadio"
+                    {...register(`guests.${index}.preferredEntree`, {
+                      required: false,
+                    })}
+                    value="BBQ Chicken"
+                  />
+                  <label className="form-check-label" htmlFor="bbqChickenRadio">
+                    BBQ Chicken
+                  </label>
+                </div>
+                <label className="mt-4">
+                  Please list any dietary restrictions:
+                </label>
+                <textarea
+                  className="form-control rounded-1"
+                  id="dietaryRestrictionsTextArea"
+                  rows={3}
+                  {...register(`guests.${index}.dietaryRestrictions`, {
+                    required: false,
+                  })}
+                ></textarea>
+              </Fragment>
+            );
+          })}
+        </div>
+
+        <div>
+          <p className="mt-4 mb-1">
+            Please limit uninvited guests to one person, but feel free to RSVP
+            invited family members as additional guests.
+          </p>
+          <button
+            className="btn border border-black rounded-1"
+            type="button"
+            onClick={() => {
+              append({
+                firstName: "",
+                lastName: "",
+                preferredEntree: "",
+                dietaryRestrictions: "",
+              });
+            }}
+          >
+            Add Guest
+          </button>
+        </div>
+
+        <button
+          className="btn border border-black rounded-1 mt-4"
+          disabled={isSubmitting}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default RsvpForm;
